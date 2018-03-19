5b3792a4a2d34066eeaf020da978b4837c4b6fc5
Sadruddin Hashmani <sadruddin.hashmani@gmail.com>
2018-03-19 01:30:58 -0400

    updated output for historical jumps

--- handlers/history.py
+++ handlers/history.py
@@ -3,7 +3,7 @@ import traceback
 from flask import request, json
 
 from handlers import APIError
-from models import db, Jump
+from models import db, Jump, Activity
 
 from sqlalchemy import func
 
@@ -15,15 +15,33 @@ def get_historical_jumps(user_id):
     if not start_date or not end_date:
         raise APIError("start_date and end_date are required query parameters", 400)
 
-    jumps = [{"date": jump[0], "count": jump[1]} for jump in _get_jumps_for_user_with_id(user_id, start_date, end_date)]
+    jumps = [
+        {"date": jump[0],
+         "count": jump[1],
+         "severity": jump[2],
+         "abduction_angle": jump[3],
+         "rpe": jump[4]
+         } for jump in _get_jumps_for_user_with_id(user_id, start_date, end_date)]
     return json.dumps({"jumps": jumps}), 200
 
 
 def _get_jumps_for_user_with_id(user_id, start_date, end_date):
     try:
-        jumps = db.session.query(Jump.jump_date, func.count(Jump.jump_date)).filter(
-            Jump.user_id == user_id, Jump.created_at >= start_date, Jump.created_at <= end_date).group_by(
-            Jump.jump_date).all()
+        jumps = db.session.query(
+            Jump.jump_date,
+            func.count(Jump.jump_date),
+            func.avg(Jump.severity),
+            func.avg(Jump.abduction_angle),
+            Activity.rpe
+        ).join(Activity).filter(
+            Jump.user_id == user_id,
+            Jump.jump_date >= start_date,
+            Jump.jump_date <= end_date
+        ).group_by(
+            Jump.jump_date,
+        ).all()
+
+        print jumps
     except Exception:
         traceback.print_exc()
         raise APIError("failed to fetch jumps for user", 500)
